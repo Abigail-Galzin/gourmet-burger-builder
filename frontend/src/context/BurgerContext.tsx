f928b0df19119c407299ad32e82960313193122a
Abigail Galzin <abigail.galzin@assuresoft.com>
2026-05-28 12:20:17 -0400

    feat: implement order creation and ingredient saving in BurgerContext

--- frontend/src/context/BurgerContext.tsx
+++ frontend/src/context/BurgerContext.tsx
@@ -1,11 +1,17 @@
 import { useState, useContext, createContext } from 'react';
-import type { Ingredients } from '../Types';
+import type { Ingredients, OrderInput } from '../Types';
+import { supabase } from '../supabaseClient';
 
 const BurgerContext = createContext(null);
 
 export function BurgerProvider({ children }) {
   const [burgerIngredients, setBurgerIngredients] = useState<Ingredients[]>([]);
   const [burgerBase, setBurgerBase] = useState<Ingredients | null>(null);
+  const [basePrice, baseCalories] = burgerBase ? [burgerBase.price, burgerBase.calories] : [0, 0];
+  const totalPrice = burgerIngredients.reduce((sum, ing) => sum + ing.price, basePrice);
+  const totalCalories = burgerIngredients.reduce(
+    (sum: number, ing: Ingredients) => sum + ing.calories, baseCalories
+  );
   const [isOrdering, setIsOrdering] = useState<boolean>(false);
 
   const addIngredient = (ingredient: Ingredients) => {
@@ -31,13 +37,57 @@ export function BurgerProvider({ children }) {
     setBurgerBase(null);
   };
 
-  const saveBurger = () => {
-    setIsOrdering(!isOrdering);
-    setBurgerIngredients([]);
-    setBurgerBase(null);
-  };
+  const saveBurger = async () => {
+    try {
+      setIsOrdering(false);
+      const orderInput: OrderInput = {
+        total_price: totalPrice,
+        total_calories: totalCalories,
+        status: 'PENDING'
+      }
 
-  const [basePrice, baseCalories] = burgerBase ? [burgerBase.price, burgerBase.calories] : [0, 0];
+      const { data: createdOrder, error } = await supabase
+        .from('orders')
+        .insert([orderInput])
+        .select('id')
+        .single();
+
+      if (error || !createdOrder) {
+        alert(`⚠️ Error al crear la orden: ${error?.message || 'Error desconocido'}`);
+        return { success: false, error: error };
+      }
+      const newOrderId = createdOrder.id;
+
+      const ingredientsPreview: Ingredients[] = burgerBase
+        ? [burgerBase, ...burgerIngredients]
+        : burgerIngredients;
+      const intermediateRows = ingredientsPreview.map((ingredient, index) => ({
+        order_id: newOrderId,
+        ingredient_id: ingredient.id,
+        layer_position: index,
+        is_base: ingredient.type.includes('base')
+      }));
+
+      const { error: ingredientsError } = await supabase
+        .from('order_ingredients')
+        .insert(intermediateRows);
+
+      if (ingredientsError) {
+        alert(`⚠️ Orden creada (#${newOrderId}), pero falló al guardar los ingredientes: ${ingredientsError.message}`);
+        return;
+      }
+      if (createdOrder != null) {
+        alert('¡✅ Orden creada con éxito!');
+        setBurgerIngredients([]);
+        setBurgerBase(null);
+      }
+    } catch (error) {
+      console.error('Error inesperado:', error);
+    } finally {
+      setIsOrdering(false);
+    }
+  }
+
   const value = {
     burgerIngredients,
     burgerBase,
@@ -45,17 +95,15 @@ export function BurgerProvider({ children }) {
     removeIngredient,
     resetBurger,
     saveBurger,
-    totalPrice: burgerIngredients.reduce((sum, ing) => sum + ing.price, basePrice),
-    totalCalories: burgerIngredients.reduce(
-      (sum: number, ing: Ingredients) => sum + ing.calories, baseCalories
-    ),
-    totalIngredients: burgerBase ? [burgerBase,...burgerIngredients]:[...burgerIngredients],
+    totalPrice,
+    totalCalories,
+    totalIngredients: burgerBase ? [burgerBase, ...burgerIngredients] : [...burgerIngredients],
     isOrdering
   };
 
   return (
-    <BurgerContext.Provider value={ value }>
-      {children} {}
+    <BurgerContext.Provider value={value}>
+      {children} { }
     </BurgerContext.Provider>
   );
 }
--- frontend/src/supabaseClient.ts
+++ frontend/src/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
-console.log(import.meta.env);
+
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Faltan las variables de entorno de Supabase en .env.local')
 }
